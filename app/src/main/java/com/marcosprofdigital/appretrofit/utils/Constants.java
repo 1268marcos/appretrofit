04a2ae33a7f4e03b901048b36e82c36d1697a92b
1268marcos <marcosantonio@ymail.com>
2022-02-27 17:06:46 -0300

    Initial Commit

--- app/src/main/java/com/marcosprofdigital/appretrofit/utils/Constants.java
+++ app/src/main/java/com/marcosprofdigital/appretrofit/utils/Constants.java
@@ -1,10 +1,13 @@
 package com.marcosprofdigital.appretrofit.utils;
+
+import com.marcosprofdigital.appretrofit.BuildConfig;
+
 // 02
 public class Constants {
 
     public static final String BASE_URL = "https://api.themoviedb.org";
 
-    public static final String API_KEY = "47e5804e0acb2aad0454f0abf2752b71";
+    public static final String API_KEY = BuildConfig.API_KEY;
 
     public static final String IMAGE_URL = "https://image.tmdb.org/t/p/w500/";
 
